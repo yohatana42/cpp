69e4b62a086d30dc65d47230181f4793a1222335
Yoshiko Hatanaka <yohatana@c6r3s4.42tokyo.jp>
2025-09-07 15:12:17 +0900

    done

--- cpp05/ex02/Bureaucrat.cpp
+++ cpp05/ex02/Bureaucrat.cpp
@@ -110,8 +110,16 @@ void Bureaucrat::signForm(AForm& form)
 void Bureaucrat::executeForm(AForm const & form) const
 {
 
-	if (form.execute(*this))
-		std::cout << _name << " executed " << form.getName() << std::endl;
+	try
+	{
+		if (form.execute(*this))
+			std::cout << _name << " executed " << form.getName() << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
 }
 
 std::string Bureaucrat::print() const
--- cpp05/ex02/main.cpp
+++ cpp05/ex02/main.cpp
@@ -20,6 +20,7 @@ int main()
 	ShrubberyCreationForm scf("SCF");
 	RobotomyRequestForm rrf("RRF");
 	PresidentialPardonForm ppf("PPF");
+	std::cout << "==== basic ====" << std::endl;
 	try
 	{
 		bur3.signForm(scf);
@@ -34,38 +35,88 @@ int main()
 		std::cerr << e.what() << '\n';
 	}
 
-	ShrubberyCreationForm scf_e("scf_e");
-	RobotomyRequestForm rrf_e("rrf_e");
-	PresidentialPardonForm ppf_e("ppf_e");
+	ShrubberyCreationForm scf_e("scf_sign");
+	RobotomyRequestForm rrf_e("rrf_sign");
+	PresidentialPardonForm ppf_e("ppf_sign");
 
+	std::cout << std::endl;
+	std::cout << "==== signed error ====" << std::endl;
 	// scf error
-	Bureaucrat err_scf_sign("err_scf", 138);
+	Bureaucrat err_scf_sign("err_scf", 149);
 	try
 	{
 		err_scf_sign.signForm(scf_e);
-		err_scf_sign.executeForm(scf_e);
 	}
 	catch(const std::exception& e)
 	{
 		std::cerr << e.what() << '\n';
 	}
 
-	Bureaucrat err_rff("err_rrf", 46);
+	Bureaucrat err_rff("err_rrf", 73);
 	try
 	{
 		err_rff.signForm(rrf_e);
-		err_rff.executeForm(rrf_e);
 	}
 	catch(const std::exception& e)
 	{
 		std::cerr << e.what() << '\n';
 	}
 
-	Bureaucrat err_ppf("err_ppf", 6);
+	Bureaucrat err_ppf("err_ppf", 26);
 	try
 	{
 		err_ppf.signForm(ppf_e);
-		err_ppf.executeForm(ppf_e);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
+	// execute error
+	std::cout << std::endl;
+	std::cout << "==== execute error ====" << std::endl;
+	Bureaucrat test1("test1", 140);
+	ShrubberyCreationForm scf_exec("scf_exec");
+
+	try
+	{
+		test1.signForm(scf_exec);
+		test1.executeForm(scf_exec);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
+	Bureaucrat test2("test2", 60);
+	RobotomyRequestForm rrf_exec("rrf_exec");
+	try
+	{
+		test2.signForm(rrf_exec);
+		test2.executeForm(rrf_exec);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
+	Bureaucrat test3("test3", 10);
+	PresidentialPardonForm ppf_exec("ppf_exec");
+	try
+	{
+		test3.signForm(ppf_exec);
+		test3.executeForm(ppf_exec);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
+	Bureaucrat test4("test4", 20);
+	ShrubberyCreationForm scf_nosign("nosign");
+	try
+	{
+		test4.executeForm(scf_nosign);
 	}
 	catch(const std::exception& e)
 	{
