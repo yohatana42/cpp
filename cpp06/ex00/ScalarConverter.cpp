4d927b9ef7ae946e15749686ddd2e08e01bf40bd
Yoshiko Hatanaka <yohatana@c6r3s5.42tokyo.jp>
2025-09-15 18:29:19 +0900

    aaaaa

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -23,68 +23,27 @@ void ScalarConverter::convert(std::string str)
 	double double_val = 0.0;
 	float float_val = 0.0f;
 
-	// ss << str;
-	// while (ss.eof())
-	// 	ss >> char_val;
-	// std::cout << char_val << std::endl;
-
-	ss << str;
-	ss >> int_val;
-	if (ss.fail())
+	if (str.size() == 1 && isalpha(str[0]))
 	{
-		std::cerr << "ss fail" << std::endl;
+		ss << str;
+		ss >> char_val;
+		int_val = static_cast<int>(char_val);
+		CastUtil::print_all(char_val, int_val, double_val, float_val);
 	}
-	while (ss.eof()|| ss.fail())
-		ss >> char_val;
-	ss.clear();
-
-	ss << str;
-	ss >> double_val;
-	while (ss.eof() || ss.fail())
-		ss >> char_val;
-	ss.clear();
-
-	ss << str;
-	ss >> float_val;
-	while (ss.eof())
-		ss >> char_val;
-
-		// ????????
-// 	c6r3s5% ./ex00 49.1
-// char: Non displayable
-// int: 49
-// float: 0.149f
-// double: 0.149
+	else if (str.find('.') == std::string::npos)
+	{
+		ss << str;
+		ss >> int_val;
+		if (ss.fail())
+			std::cerr << "ss fail" << std::endl;
+		// intの場合
+		char_val = static_cast<char>(int_val);
+		double_val = static_cast<double>(int_val);
+		float_val = static_cast<float>(int_val);
+		CastUtil::print_all(char_val, int_val, double_val, float_val);
+	}
 
 
-	CastUtil::print_all(char_val, int_val, double_val, float_val);
-
-	// if (str.size() == 1 && isalpha(str[0]))
-	// {
-	// 	print_char = str[0];
-	// 	print_int = print_char;
-	// 	std::cout << print_int << std::endl;
-	// 	print_double = print_char;
-	// 	print_float = print_char;
-	// 	return (CastUtil::print_all(print_char, print_int, print_double, print_float));
-	// }
-
-	// for (int i = 0;str[i] != '\0';i++)
-	// {
-	// 	if (isdigit(str[i]) || str[i] == '.' || (str[i] == 'f' && i == (int)str.size() - 1)
-	// 	|| (i == 0 && (str[i] == '-' || str[i] == '+')))
-	// 		continue ;
-	// 	else
-	// 		return CastUtil::print_all("impossible", "impossible", "impossible", "impossible");
-	// }
-
-	// // intの場合
-	// // 2147483647 10桁
-	// print_int = static_cast<int>(str);
-	// print_char = print_int;
-	// print_double = print_int;
-	// print_float = print_int;
-	// return CastUtil::print_all("impossible", "impossible", "impossible", "impossible");
 }
 
 /*
