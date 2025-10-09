cca5c83835db6f7673ccd27922d62a28ef8e9384
Yoshiko Hatanaka <yohatana@c6r4s3.42tokyo.jp>
2025-10-09 18:54:23 +0900

    move~?

--- cpp06/ex01/Serializer.cpp
+++ cpp06/ex01/Serializer.cpp
@@ -14,4 +14,18 @@ Serializer *Serializer::operator=(const Serializer *src)
 
 Serializer::~Serializer() {}
 
+uintptr_t serialize(Data* ptr)
+{
+	uintptr_t result;
 
+	result = reinterpret_cast<uintptr_t>(ptr);
+	return (result);
+}
+
+Data* deserialize(uintptr_t raw)
+{
+	Data *result;
+
+	result = reinterpret_cast<Data *>(raw);
+	return (result);
+}
--- cpp06/ex01/main.cpp
+++ cpp06/ex01/main.cpp
@@ -1,5 +1,13 @@
+#include "Serializer.hpp"
+
 int main()
 {
+	Data *data;
+	Data *result_data;
+	uintptr_t ptr;
+	uintptr_t result_ptr;
 
+	result_ptr = Serializer::serialize(data);
+	result_data = Serializer::deserialize(ptr);
 	return (0);
 }
