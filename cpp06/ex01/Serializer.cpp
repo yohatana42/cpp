badd1beadb2e50b1ea974193852c82f286c29575
Yoshiko Hatanaka <yohatana@c6r4s3.42tokyo.jp>
2025-10-09 18:39:43 +0900

    start ex01

---
+++ cpp06/ex01/Serializer.cpp
@@ -0,0 +1,17 @@
+#include "Serializer.hpp"
+
+Serializer::Serializer() {}
+
+Serializer::Serializer(const Serializer *src)
+{
+	(void)src;
+}
+
+Serializer *Serializer::operator=(const Serializer *src)
+{
+	(void)src;
+}
+
+Serializer::~Serializer() {}
+
+
---
+++ cpp06/ex01/Serializer.hpp
@@ -0,0 +1,23 @@
+#ifndef SERIALIZER_HPP
+ #define SERIALIZER_HPP
+
+ #include <stdint.h>
+
+typedef struct s_Data
+{
+	int num;
+} Data;
+
+class Serializer
+{
+	public :
+		static uintptr_t serialize(Data* ptr);
+		static Data* deserialize(uintptr_t raw);
+	private :
+		Serializer();
+		Serializer(const Serializer* src);
+		Serializer *operator=(const Serializer* src);
+		~Serializer();
+};
+
+#endif
