a4e3787d5f29f4a5c02331cda43046bd7bfb878b
Yoshiko Hatanaka <yohatana@c6r3s7.42tokyo.jp>
2025-10-14 14:46:47 +0900

    add ex00

---
+++ cpp07/ex00/whatever.hpp
@@ -0,0 +1,37 @@
+#ifndef WHATEVER_HPP
+ #define WHATEVER_HPP
+
+#include <iostream>
+
+template<typename T>
+void swap(T& a,T& b)
+{
+	T temp;
+	temp = a;
+	a = b;
+	b = temp;
+}
+
+template<typename T>
+T& min(T& a,T& b)
+{
+	if (a == b)
+		return (b);
+	if (a < b)
+		return (a);
+	else
+		return (b);
+}
+
+template<typename T>
+T& max(T& a,T& b)
+{
+	if (a == b)
+		return (b);
+	if (a < b)
+		return (b);
+	else
+		return (a);
+}
+
+#endif
