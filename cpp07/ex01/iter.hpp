dd34f0cd14ab14024fd46bd1c76da43da44dc513
Yoshiko Hatanaka <yohatana@c6r3s7.42tokyo.jp>
2025-10-14 19:10:18 +0900

    add

---
+++ cpp07/ex01/iter.hpp
@@ -0,0 +1,15 @@
+#ifndef ITER_HPP
+ #define ITER_HPP
+
+template<typename T>
+void iter(T *a,const int length, const (* p_func))(a))
+{
+	T temp;
+	temp = a;
+	a = b;
+	b = temp;
+}
+
+void func(T a);
+
+#endif
