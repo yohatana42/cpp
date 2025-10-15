9ab69ac2f5df7989b789f8891100c78729df4dad
Yoshiko Hatanaka <yohatana@c6r3s7.42tokyo.jp>
2025-10-15 16:04:04 +0900

    ok

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -17,6 +17,31 @@ ScalarConverter::~ScalarConverter() {}
 
 void ScalarConverter::convert(std::string str)
 {
+	if (str == "nan" || str == "nanf")
+	{
+		std::cout << "char: impossible" << std::endl;
+		std::cout << "int: impossible" << std::endl;
+		std::cout << "float: nanf" << std::endl;
+		std::cout << "double: nan" <<  std::endl;
+		return ;
+	}
+	else if (str == "-inf" || str == "-inff")
+	{
+		std::cout << "char: impossible" << std::endl;
+		std::cout << "int: impossible" << std::endl;
+		std::cout << "float: -inff" << std::endl;
+		std::cout << "double: -inf" <<  std::endl;
+		return ;
+	}
+	else if (str == "+inf" || str == "+inff")
+	{
+		std::cout << "char: impossible" << std::endl;
+		std::cout << "int: impossible" << std::endl;
+		std::cout << "float: +inff" << std::endl;
+		std::cout << "double: +inf" <<  std::endl;
+		return ;
+	}
+
 	if (!ScalarConverter::validate_input(str))
 	{
 		ScalarConverter::print_impossible();
@@ -28,16 +53,10 @@ void ScalarConverter::convert(std::string str)
 		return ;
 	}
 
-	// 一回全部doubleにする
-	// char c_val = 0;
-	// int int_val = 0;
-	// float f_val = 0;
 	double d_val = 0;
 	char* end;
 
     d_val = std::strtod(str.c_str(), &end);
-	std::cout << "=== " << d_val << " ===" << std::endl;
-
 	if (str.size() == 1 && isalpha(str[0]))
 		return (ScalarConverter::print_char(str));
 
@@ -54,32 +73,7 @@ void ScalarConverter::convert(std::string str)
 		|| str == "2147483647" || str == "-2147483648"))
 		return (ScalarConverter::print_int(str));
 
-	// if (d_val < std::numeric_limits<char>::min() || d_val > std::numeric_limits<char>::max())
-	// 		std::cout << "char: immposible" << std::endl;
-	// else
-	// {
-	// 	c_val = static_cast<char>(d_val);
-	// 	if (isprint(c_val))
-	// 		std::cout << "char: " << c_val << std::endl;
-	// 	else
-	// 		std::cout << "char: Non displayable" << std::endl;
-	// }
-	// if (d_val < std::numeric_limits<int>::min() || d_val > std::numeric_limits<int>::max())
-	// 	std::cout << "int :immposible" << std::endl;
-	// else
-	// {
-	// 	int_val = static_cast<int>(d_val);
-	// 	std::cout << "int :" << int_val << std::endl;
-	// }
-
-	// if (d_val < std::numeric_limits<float>::min() || d_val > std::numeric_limits<float>::max())
-	// 	std::cout << "float :immposible" << std::endl;
-	// else
-	// {
-	// 	f_val = static_cast<float>(d_val);
-	// 	std::cout << "float :" << f_val << std::endl;
-	// }
-	// std::cout << "double :" << d_val << std::endl;
+	ScalarConverter::print_float_and_double(d_val);
 
 }
 
@@ -93,7 +87,7 @@ bool ScalarConverter::validate_input(std::string str)
 	{
 		if (isdigit(str[i]))
 			continue ;
-		else if (i == 0 && str[i] == '-')
+		else if (i == 0 && (str[i] == '-' || str[i] == '+'))
 			continue ;
 		else if (i == (int)str.size() - 1 && str[i] == 'f')
 			continue ;
@@ -146,10 +140,10 @@ void ScalarConverter::print_char(std::string str)
 	int_val = static_cast<int>(c_val);
 	f_val = static_cast<float>(c_val);
 	d_val = static_cast<double>(c_val);
-	std::cout << "char :" << c_val << std::endl;
-	std::cout << "int :" << int_val << std::endl;
-	std::cout << "float :" << f_val << std::endl;
-	std::cout << "double :" << d_val << std::endl;
+	std::cout << "char: " << c_val << std::endl;
+	std::cout << "int: " << int_val << std::endl;
+	std::cout << "float: " << f_val << std::fixed << std::setprecision(1) << "f" << std::endl;
+	std::cout << "double: " << d_val << std::fixed << std::setprecision(1) << std::endl;
 }
 
 void ScalarConverter::print_int(std::string str)
@@ -166,22 +160,54 @@ void ScalarConverter::print_int(std::string str)
 	f_val = static_cast<float>(int_val);
 	d_val = static_cast<double>(int_val);
 	if (isprint(c_val))
-		std::cout << "char :" << c_val << std::endl;
-	else if (0 < int_val && int_val < 128)
-		std::cout << "char : Non displayable" << std::endl;
+		std::cout << "char: " << c_val << std::endl;
+	else if (0 < int_val && int_val < 127)
+		std::cout << "char: Non displayable" << std::endl;
 	else
 		std::cout << "char: impossible" << std::endl;
-	std::cout << "int :" << int_val << std::endl;
-	std::cout << "float :" << f_val << std::endl;
-	std::cout << "double :" << d_val << std::endl;
+	std::cout << "int: " << int_val << std::endl;
+	std::cout << "float: " << std::fixed << std::setprecision(1) << f_val  << "f" << std::endl;
+	std::cout << "double: " << std::fixed << std::setprecision(1) << d_val << std::endl;
 }
 
- /*
+void ScalarConverter::print_float_and_double(double d_val)
+{
+	char c_val = 0;
+	int int_val = 0;
+	float f_val = 0;
+	if (d_val < std::numeric_limits<int>::min()
+		|| d_val > std::numeric_limits<int>::max())
+		std::cout << "char: impossible" << std::endl;
+	else
+	{
+		int_val = static_cast<int>(d_val);
+		c_val = static_cast<char>(int_val);
+		if (0 > int_val || int_val > 127)
+			std::cout << "char: impossible" << std::endl;
+		else if (isprint(c_val))
+			std::cout << "char: " << c_val << std::endl;
+		else
+			std::cout << "char: Non displayable" << std::endl;
+	}
+	if (d_val < std::numeric_limits<int>::min()
+		|| d_val > std::numeric_limits<int>::max())
+		std::cout << "int: impossible" << std::endl;
+	else
+	{
+		int_val = static_cast<int>(d_val);
+		std::cout << "int: " << int_val << std::endl;
+	}
 
- //test case
-int max
-2147483647
+	if ((d_val > 0 && d_val > std::numeric_limits<float>::max())
+		|| (d_val < 0 && -d_val > std::numeric_limits<float>::max()))
+		std::cout << "float: impossible" << std::endl;
+	else
+	{
+		f_val = static_cast<float>(d_val);
+		// std::cout << "float: "<< f_val << "f" << std::endl;
+		std::cout << "float: "<< std::fixed << std::setprecision(1) << f_val << "f" << std::endl;
+	}
+	// std::cout << "double: " << d_val << std::endl;
+	std::cout << "double: " << std::fixed << std::setprecision(1) << d_val << std::endl;
+}
 
-double max
-179769313486231570000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
-*/
--- cpp06/ex00/ScalarConverter.hpp
+++ cpp06/ex00/ScalarConverter.hpp
@@ -7,8 +7,6 @@
  #include <stdlib.h>
  #include <cstdlib>
  #include <iomanip>
- #include <limits>
- #include "CastUtil.hpp"
  #include <cerrno>
  #include <limits>
 
@@ -18,17 +16,18 @@
 class ScalarConverter
 {
 	public:
-		ScalarConverter& operator=(const ScalarConverter& src);
-		~ScalarConverter();
 		static void convert(std::string str);
 	private:
 		ScalarConverter();
 		ScalarConverter(const ScalarConverter& src);
+		ScalarConverter& operator=(const ScalarConverter& src);
+		~ScalarConverter();
 		static bool validate_input(std::string str);
 		static bool validate_overflow(std::string str);
 		static void	print_impossible();
 		static void print_char(std::string str);
 		static void print_int(std::string str);
+		static void print_float_and_double(double d_val);
 
 };
 
