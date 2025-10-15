e02a94ad0c657b63794a467ee14ac6cf1e885ecc
Yoshiko Hatanaka <yohatana@c6r3s7.42tokyo.jp>
2025-10-15 20:16:14 +0900

    add cpp08wx00

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -4,5 +4,17 @@
  #include <string>
  #include <iostream>
  #include <vector>
+ #include <exception>
+ #include <algorithm>
+ #include <list>
+
+template <typename T>
+typename T::iterator easyfind(T& cont, int num)
+{
+	typename T::iterator it = std::find(cont.begin(), cont.end(), num);
+	if (it == cont.end())
+		throw std::runtime_error("no such number");
+	return (it);
+}
 
 #endif
--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -1,17 +1,37 @@
 #include "easyfind.hpp"
-#include "easyfind.tpp"
 
 int main()
 {
-	int sum = add(1, 2);
-	std::cout << sum << std::endl;
-
-	int result = 0;
+	std::vector<int>::iterator result;
 	std::vector<int> v;
+
 	v.push_back(1);
 	v.push_back(4);
 	v.push_back(2);
-	result = easyfind(v, 2);
-	std::cout << "result :" << result << std::endl;
+	try
+	{
+		result = ::easyfind(v, 2);
+		std::cout << "result :" << *result << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
+	std::list<int>::iterator result2;
+	std::list<int> list;
+	list.push_back(1);
+	list.push_back(1);
+	list.push_back(1);
+	try
+	{
+		result2 = ::easyfind(list, 2);
+		std::cout << "result :" << *result << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
 	return (0);
 }
